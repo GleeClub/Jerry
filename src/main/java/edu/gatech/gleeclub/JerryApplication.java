3b187bad0179ece36d6c7d1b315f2a27191515dd
Keith Cartledge <kcartledge3@gatech.edu>
2017-02-07 00:09:10 -0500

    Add base Spring project

---
+++ src/main/java/edu/gatech/gleeclub/JerryApplication.java
@@ -0,0 +1,12 @@
+package edu.gatech.gleeclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerryApplication.class, args);
+	}
+}
